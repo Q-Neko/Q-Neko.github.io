0515e5348951b5a04d353a0a9c94a62ff039938b
Joonas Nivala <joonas.nivala@csc.fi>
2026-06-23 10:38:08 +0300

    embed liana initial

--- src/components/ui/NewsletterFormCard.jsx
+++ src/components/ui/NewsletterFormCard.jsx
@@ -1,6 +1,5 @@
 
-import { set } from 'astro:schema';
-import { useState, useRef, useEffect } from 'react';
+import { useState } from 'react';
 
 export const NewsletterFormCard = ({ t }) => {
 
@@ -42,36 +41,32 @@ export const NewsletterFormCard = ({ t }) => {
     }
 
     const handleSubmit = (e) => {
-        e.preventDefault();
-
-        if (validateForm()) {
-            //TODO: handle form submission, e.g. send data to backend or email service
-            console.log('Form submitted:', formData);
-
-            setFormData({
-                email: '',
-                privacyPolicyAccepted: false,
-            });
-
-            setSubmittedNotification(true);
-
-            setTimeout(() => {
-                setSubmittedNotification(false);
-            }, 3000);
+        if (!validateForm()) {
+            e.preventDefault();
+            return;
         }
 
-        else {
-            console.log('Form has errors:', formErrors);
-        }
+        setFormData({
+            email: '',
+            privacyPolicyAccepted: false,
+        });
+
+        setSubmittedNotification(true);
+
+        setTimeout(() => {
+            setSubmittedNotification(false);
+        }, 3000);
     };
 
     return (
         <div className="base-card opacity-0 animate-fadeUp1 max-w-2xl flex flex-col gap-4">
 
-            <form className='flex flex-col gap-6' onSubmit={handleSubmit}>
+            <iframe name="newsletter-frame" title="Newsletter subscription" style={{ display: 'none' }} />
+            <form method="post" action="https://uutiskirje.asiakkaandomain.fi/account" target="newsletter-frame" id="lianamailer" className='lianamailer flex flex-col gap-6' onSubmit={handleSubmit}>
                 <div>
                     <label className="form-label">{t.pageContent.newsletter.emailLabel}</label>
                     <input
+                        name="email"
                         type="text"
                         placeholder={t.pageContent.newsletter.emailPlaceholder}
                         className="form-input"
@@ -80,16 +75,29 @@ export const NewsletterFormCard = ({ t }) => {
                     />
                     {formErrors.email && <p className="form-error">{formErrors.email}</p>}
 
-                    <div className="flex mt-2 gap-2 items-center">
-                        <input type="checkbox" className="form-checkbox" checked={formData.privacyPolicyAccepted} onChange={(e) => handleFormDataChange('privacyPolicyAccepted', e.target.checked)} />
-                        <p>{t.pageContent.contact.form.privacyPolicyText} <a href={t.pageContent.contact.form.privacyPolicyLink} className="text-blue hover:underline">
-                            {t.pageContent.contact.form.privacyPolicyLinkText}
-                        </a></p>
+                    <input type="hidden" name="join[]" value="1646430"></input>
+
+                    <div className="flex flex-col mt-2 gap-2">
+                        <div className="flex gap-2">
+                            <input type="checkbox" className="form-checkbox" checked={formData.privacyPolicyAccepted} onChange={(e) => handleFormDataChange('privacyPolicyAccepted', e.target.checked)} />
+                            <p>{t.pageContent.contact.form.privacyPolicyText} <a href={t.pageContent.contact.form.privacyPolicyLink} className="text-blue hover:underline">
+                                {t.pageContent.contact.form.privacyPolicyLinkText}
+                            </a></p>
+                        </div>
+
+                        {
+                            //TODO: replace with actual consent from Liana. Translate
+                        }
+                        <div className="flex gap-2">
+                            <input type="checkbox" name="consent[]" value="45-2-fi" className="form-checkbox" />
+                            <p>Liana Technologies saa lähettää minulle digitaaliseen markkinointiin ja viestintään liittyvää sisältöä.</p>
+                        </div>
+
                     </div>
                     {formErrors.privacyPolicyAccepted && <p className="form-error">{formErrors.privacyPolicyAccepted}</p>}
                 </div>
-                
-                <button disabled className="btn-primary self-start disabled:cursor-not-allowed">{t.pageContent.newsletter.subscribeCta}</button>
+
+                <input value={t.pageContent.newsletter.subscribeCta} type="submit" disabled className="btn-primary self-start disabled:cursor-not-allowed" />
                 {submittedNotification && <p className="text-green-600 mt-2">{t.pageContent.newsletter.submitSuccess}</p>}
             </form>
         </div>
