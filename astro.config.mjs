9043508fa2c87adc72a9620b6eef6460716ca4e1
Joonas Nivala <joonas.nivala@csc.fi>
2026-06-25 14:55:34 +0300

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,15 +12,13 @@ import pagefind from "astro-pagefind";
 
 import logoZip from "./src/integrations/logoZip.mjs";
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://q-neko.com',
 
-  integrations: [
-    react(),
-    pagefind(),
-    logoZip(),
-  ],
+  integrations: [react(), pagefind(), logoZip(), sitemap()],
 
   markdown: {
     remarkPlugins: [remarkMath],
